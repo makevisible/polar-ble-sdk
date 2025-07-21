5268764f3eeb39ae88078504d35a833b458d0e9e
Maciej Korzeniewski <mck.korzeniewski@gmail.com>
2025-07-21 11:17:55 +0300

    add more logs around getSleepRecordingState method

--- sources/iOS/ios-communications/Sources/PolarBleSdk/sdk/impl/PolarBleApiImpl+Sleep.swift
+++ sources/iOS/ios-communications/Sources/PolarBleSdk/sdk/impl/PolarBleApiImpl+Sleep.swift
@@ -39,26 +39,62 @@ extension PolarBleApiImpl: PolarSleepApi {
         }
     }
     
+
     func getSleepRecordingState(identifier: String) -> Single<Bool> {
-            
+        BleLogger.trace("getSleepRecordingState: called for identifier: \(identifier)")
         let observeRecordingState = observeSleepRecordingState(identifier: identifier)
-            .filter { $0.isEmpty == false }
+            .do(
+                onNext: { value in
+                    BleLogger.trace("getSleepRecordingState: Received value: \(value)")
+                },
+                onError: { error in
+                    BleLogger.trace("getSleepRecordingState: error: \(error)")
+                },
+                onSubscribe: {
+                    BleLogger.trace("getSleepRecordingState: Subscribed to observeSleepRecordingState")
+                },
+                onDispose: {
+                    BleLogger.trace("getSleepRecordingState: onDispose")
+                }
+            )
+            .filter { !$0.isEmpty }
             .take(1)
             .map { $0.last! }
             .asSingle()
-        
         return observeRecordingState
     }
         
     func observeSleepRecordingState(identifier: String) -> Observable<[Bool]> {
-        let receiveSleepRecordingStates:Observable<[SleepRecordingStateWrapper]> =
+        BleLogger.trace("observeSleepRecordingState: called for identifier: \(identifier)")
+        let receiveSleepRecordingStates: Observable<[SleepRecordingStateWrapper]> =
             self.receiveRestApiEvents(identifier: identifier)
-        let receiveSleepRecordingEnabled = receiveSleepRecordingStates.compactMap {
-            return $0.compactMap { $0.sleepRecordingState.isEnabled }
-        }
+        let receiveSleepRecordingEnabled = receiveSleepRecordingStates
+            .do(
+                onNext: { value in
+                    BleLogger.trace("observeSleepRecordingState: Received event payloads: \(value)")
+                },
+                onError: { error in
+                    BleLogger.trace("observeSleepRecordingState: Error: \(error)")
+                },
+                onSubscribe: {
+                    BleLogger.trace("observeSleepRecordingState: Subscribed to receiveRestApiEvents")
+                },
+                onDispose: {
+                    BleLogger.trace("observeSleepRecordingState: onDispose")
+                }
+            )
+            .compactMap {
+                let boolArray = $0.compactMap { $0.sleepRecordingState.isEnabled }
+                BleLogger.trace("observeSleepRecordingState: Mapped to Boolean array: \(boolArray)")
+                return boolArray
+            }
         let subscribe = self.putNotification(identifier: identifier, notification: "{}",
                              path: "/REST/SLEEP.API?cmd=subscribe&event=sleep_recording_state&details=[enabled]")
-        return subscribe.andThen(receiveSleepRecordingEnabled)
+        return subscribe
+            .do(onCompleted: {
+                BleLogger.trace("observeSleepRecordingState: Subscription notification sent")
+            })
+            .andThen(receiveSleepRecordingEnabled)
     }
     
     func getSleepData(identifier: String, fromDate: Date, toDate: Date) -> Single<[PolarSleepData.PolarSleepAnalysisResult]> {
