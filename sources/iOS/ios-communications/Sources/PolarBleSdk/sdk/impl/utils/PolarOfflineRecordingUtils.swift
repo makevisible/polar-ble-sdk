c3a0d5844f9bcee848960c3de5752dbd1e31b293
Maciej Korzeniewski <mck.korzeniewski@gmail.com>
2025-12-09 11:22:24 +0200

    Bring back try-catch for listOfflineRecordingsV1 on iOS

--- sources/iOS/ios-communications/Sources/PolarBleSdk/sdk/impl/utils/PolarOfflineRecordingUtils.swift
+++ sources/iOS/ios-communications/Sources/PolarBleSdk/sdk/impl/utils/PolarOfflineRecordingUtils.swift
@@ -33,58 +33,59 @@ class PolarOfflineRecordingUtils {
             entry == "R/" ||
             entry.contains(".REC")
         }
+        /// If mapping single entry fails, we return empty observable so that
+        /// we can continue to the next entry.
         .flatMap { (path, size) -> Observable<PolarOfflineRecordingEntry> in
+            do {
+                guard path.matches("^(\\/)U(\\/)0(\\/)([0-9]{8})(\\/)R(\\/)([0-9]{6})(\\/)([A-Z]+[0-9]*)\\.REC") else {
+                    BleLogger.error("Listing offline recording failed. Unexpected format for entry: \(path)")
+                    return Observable.empty()
+                }
 
-            guard path.matches("^(\\/)U(\\/)0(\\/)([0-9]{8})(\\/)R(\\/)([0-9]{6})(\\/)([A-Z]+[0-9]*)\\.REC") else {
-                return Observable.error(PolarErrors.polarBleSdkInternalException(
-                    description: "Listing offline recording failed. Unexpected format for entry: \(path)"
-                ))
+                let components = path.split(separator: "/")
+                let dateFormatter = DateFormatter()
+                dateFormatter.calendar = .init(identifier: .iso8601)
+                dateFormatter.locale = Locale(identifier: "en_US_POSIX")
+
+                guard components.count == 6 else {
+                    BleLogger.error("Listing offline recording failed. Unexpected number of path components for entry: \(path)")
+                    return Observable.empty()
+                }
+
+                if components[2].count == 8 && components[4].count == 6 {
+                    dateFormatter.dateFormat = "yyyyMMddHHmmss"
+                } else {
+                    dateFormatter.dateFormat = "yyyyMMddHHmm"
+                }
+
+                guard let date = dateFormatter.date(from: String(components[2] + components[4])) else {
+                    BleLogger.error("Listing offline recording failed. Couldn't parse create data from date \(components[2]) and time \(components[4])")
+                    return Observable.empty()
+                }
+
+                guard let pmdMeasurementType = try? OfflineRecordingUtils.mapOfflineRecordingFileNameToMeasurementType(fileName: String(components[5])) else {
+                    BleLogger.error("Listing offline recording failed. Couldn't parse the pmd type from \(components[5])")
+                    return Observable.empty()
+                }
+
+                guard let type = try? PolarDataUtils.mapToPolarFeature(from: pmdMeasurementType) else {
+                    BleLogger.error("Listing offline recording failed. Couldn't parse the polar type from pmd type: \(pmdMeasurementType)")
+                    return Observable.empty()
+                }
+
+                let entry = PolarOfflineRecordingEntry(
+                    path: path,
+                    size: size,
+                    date: date,
+                    type: type
+                )
+
+                BleLogger.trace("Adding entry: \(entry)")
+                return Observable.just(entry)
+            } catch {
+                BleLogger.error("Listing offline recording failed. Error \(error)")
+                return Observable.empty()
             }
-
-            let components = path.split(separator: "/")
-            let dateFormatter = DateFormatter()
-            dateFormatter.calendar = .init(identifier: .iso8601)
-            dateFormatter.locale = Locale(identifier: "en_US_POSIX")
-
-            guard components.count == 6 else {
-                return Observable.error(PolarErrors.polarBleSdkInternalException(
-                    description: "Listing offline recording failed. Unexpected number of path components for entry: \(path)"
-                ))
-            }
-
-            if components[2].count == 8 && components[4].count == 6 {
-                dateFormatter.dateFormat = "yyyyMMddHHmmss"
-            } else {
-                dateFormatter.dateFormat = "yyyyMMddHHmm"
-            }
-
-            guard let date = dateFormatter.date(from: String(components[2] + components[4])) else {
-                return Observable.error(PolarErrors.dateTimeFormatFailed(
-                    description: "Listing offline recording failed. Couldn't parse create data from date \(components[2]) and time \(components[4])"
-                ))
-            }
-
-            guard let pmdMeasurementType = try? OfflineRecordingUtils.mapOfflineRecordingFileNameToMeasurementType(fileName: String(components[5])) else {
-                return Observable.error(PolarErrors.polarBleSdkInternalException(
-                    description: "Listing offline recording failed. Couldn't parse the pmd type from \(components[5])"
-                ))
-            }
-
-            guard let type = try? PolarDataUtils.mapToPolarFeature(from: pmdMeasurementType) else {
-                return Observable.error(PolarErrors.polarBleSdkInternalException(
-                    description: "Listing offline recording failed. Couldn't parse the polar type from pmd type: \(pmdMeasurementType)"
-                ))
-            }
-
-            let entry = PolarOfflineRecordingEntry(
-                path: path,
-                size: size,
-                date: date,
-                type: type
-            )
-
-            BleLogger.trace("Adding entry: \(entry)")
-            return Observable.just(entry)
         }
         .groupBy { entry in
             entry.path.replacingOccurrences(of: "\\d+\\.REC$", with: ".REC", options: .regularExpression)
